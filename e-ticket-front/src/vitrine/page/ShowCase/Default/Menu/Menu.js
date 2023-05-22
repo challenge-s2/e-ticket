3a81f1493ca58c4ec77f6ee0074a1d083c1d24c3
TheoRacherR <theo.racher-raulin@outlook.com>
2023-05-22 21:32:41 +0200

    delete About Us Page

--- e-ticket-front/src/vitrine/page/ShowCase/Default/Menu/Menu.js
+++ e-ticket-front/src/vitrine/page/ShowCase/Default/Menu/Menu.js
@@ -2,17 +2,17 @@ import { React } from "react";
 import styles from "./Menu.module.scss";
 import Auth from "./Auth/Auth";
 
-const Menu = ({handleChangePage}) => {
+const Menu = () => {
   return (
     <>
       <nav className={styles.container}>
-        <div className={styles.left} onClick={() => handleChangePage("homepage")}>
+        <div className={styles.left}>
           {/* Logo */}
           eTickets
         </div>
 
         <div className={styles.middle}>
-          <div onClick={() => handleChangePage("about-us")}>Qui sommes nous ?</div>
+          {/* <div onClick={() => handleChangePage("about-us")}>Qui sommes nous ?</div> */}
         </div>
 
         <div className={styles.right}>
