7f33a46680f32ff79e7fea1c567e76a308395664
TheoRacherR <theo.racher-raulin@outlook.com>
2023-05-27 17:00:47 +0200

    start feature

--- e-ticket-front/src/vitrine/page/Application/Application.js
+++ e-ticket-front/src/vitrine/page/Application/Application.js
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./Application.module.scss";
 import Menu from "./Menu/Menu";
 import LeftBoard from "./LeftBoard/LeftBoard";
+import { Routes, Route } from "react-router-dom";
+import ListOldCommand from "./Main/Command/ListOldCommand/ListOldCommand";
 
 const Application = () => {
   return (
@@ -13,6 +15,10 @@ const Application = () => {
             <LeftBoard />
           </div>
           <div className={styles.container_main_board}>
+            <Routes>
+              <Route path="/list-old-commands" element={<ListOldCommand />} />
+              {/* <Route path="/app/new-command"/> */}
+            </Routes>
           </div>
         </div>
       </div>
---
+++ e-ticket-front/src/vitrine/page/Application/Main/Command/ListOldCommand/ListOldCommand.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./ListOldCommand.module.scss";
+import { Link, Navigate } from "react-router-dom";
+
+
+const ListOldCommand = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        list old command
+      </div>
+    </>
+  );
+};
+
+export default ListOldCommand;
