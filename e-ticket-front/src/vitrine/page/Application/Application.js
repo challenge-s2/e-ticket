ced922c4ade4fcb6f1aa8e5f736ad6732e151cb4
TheoRacherR <theo.racher-raulin@outlook.com>
2023-06-03 16:21:22 +0200

    new command : done

--- e-ticket-front/src/vitrine/page/Application/Application.js
+++ e-ticket-front/src/vitrine/page/Application/Application.js
@@ -4,12 +4,13 @@ import Menu from "./Menu/Menu";
 import LeftBoard from "./LeftBoard/LeftBoard";
 import { Routes, Route } from "react-router-dom";
 import ListOldCommand from "./Main/Command/ListOldCommand/ListOldCommand";
+import NewCommand from "./Main/Command/NewCommand/NewCommand";
 
 const Application = () => {
   return (
     <>
       <div className={styles.container}>
-        <Menu />
+        {/* <Menu /> */}
         <div className={styles.container_board}>
           <div className={styles.container_left_board}>
             <LeftBoard />
@@ -17,6 +18,7 @@ const Application = () => {
           <div className={styles.container_main_board}>
             <Routes>
               <Route path="/list-old-commands" element={<ListOldCommand/>}/>
+              <Route path="/new-command" element={<NewCommand/>}/>
             </Routes>
           </div>
         </div>
