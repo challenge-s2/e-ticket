c9bf10d26b8dc27e61523dd7dbc4fd609eee808d
TheoRacherR <theo.racher-raulin@outlook.com>
2023-06-13 00:10:26 +0200

    Change file architecture + My information page

--- e-ticket-front/src/App.js
+++ e-ticket-front/src/App.js
@@ -1,36 +1,40 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import ShowCase from "./vitrine/page/ShowCase/ShowCase";
-import Auth from "./vitrine/page/Auth/Auth";
-import Application from "./vitrine/page/Application/Application";
-import ListOldCommand from "./vitrine/page/Application/Main/Command/ListOldCommand/ListOldCommand";
-import NewCommand from "./vitrine/page/Application/Main/Command/NewCommand/NewCommand";
-import ListOfProducts from "./vitrine/page/Application/Main/Product/ListOfProducts/ListOfProducts";
-import NewProduct from './vitrine/page/Application/Main/Product/NewProduct/NewProduct'
-import EditProduct from "./vitrine/page/Application/Main/Product/EditProduct/EditProduct";
-import DetailOldCommand from "./vitrine/page/Application/Main/Command/DetailOldCommand/DetailOldCommand";
+import ShowCase from "./pages/Vitrine/ShowCase";
+import Auth from "./pages/Auth/Auth";
+import Application from "./pages/Application/Application";
+import ListOldCommand from "./pages/Application/Main/Command/ListOldCommand/ListOldCommand";
+import NewCommand from "./pages/Application/Main/Command/NewCommand/NewCommand";
+import ListOfProducts from "./pages/Application/Main/Product/ListOfProducts/ListOfProducts";
+import NewProduct from "./pages/Application/Main/Product/NewProduct/NewProduct";
+import EditProduct from "./pages/Application/Main/Product/EditProduct/EditProduct";
+import DetailOldCommand from "./pages/Application/Main/Command/DetailOldCommand/DetailOldCommand";
+import MyInformations from "./pages/Application/Main/Settings/MyInformations/MyInformations";
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <Routes>
-
           <Route path="/" exact element={<ShowCase />} />
 
           <Route path="/auth" exact element={<Auth />} />
 
           <Route path="/app" element={<Application />}>
             <Route path="list-old-commands" element={<ListOldCommand />} />
-            <Route path="new-command" element={<NewCommand/>}/>
-            <Route path="detail-old-command/:id" element={<DetailOldCommand/>}/>
+            <Route path="new-command" element={<NewCommand />} />
+            <Route
+              path="detail-old-command/:id"
+              element={<DetailOldCommand />}
+            />
 
-            <Route path="list-products" element={<ListOfProducts/>}/>
-            <Route path="new-product" element={<NewProduct/>}/>
-            <Route path="edit-product/:id" element={<EditProduct/>}/>
+            <Route path="list-products" element={<ListOfProducts />} />
+            <Route path="new-product" element={<NewProduct />} />
+            <Route path="edit-product/:id" element={<EditProduct />} />
+
+            <Route path="my-informations" element={<MyInformations />} />
           </Route>
-
         </Routes>
       </div>
     </Router>
---
+++ e-ticket-front/src/pages/Application/Main/Command/DetailOldCommand/DetailOldCommand.js
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from "react";
+import styles from "./DetailOldCommand.module.scss";
+import { useParams } from "react-router-dom";
+
+const products = [
+  //Exemple TODO > connexion avec la BDD
+  {
+    id: 1,
+    name: "Banane",
+    price: 1.98,
+  },
+  {
+    id: 2,
+    name: "Pomme",
+    price: 0.8,
+  },
+  {
+    id: 3,
+    name: "Kiwi",
+    price: 1.88,
+  },
+  {
+    id: 4,
+    name: "Cerise",
+    price: 1.92,
+  },
+  {
+    id: 5,
+    name: "Cerise",
+    price: 1.92,
+  },
+  {
+    id: 6,
+    name: "Cerise",
+    price: 1.92,
+  },
+  {
+    id: 7,
+    name: "Poire",
+    price: 1.8,
+  },
+  {
+    id: 8,
+    name: "Orange",
+    price: 2.08,
+  },
+  {
+    id: 9,
+    name: "Cerise",
+    price: 1.92,
+  },
+  {
+    id: 10,
+    name: "Cerise",
+    price: 1.92,
+  },
+  {
+    id: 11,
+    name: "Cerise",
+    price: 1.92,
+  },
+  {
+    id: 12,
+    name: "Cerise",
+    price: 1.92,
+  },
+  {
+    id: 13,
+    name: "Cerise",
+    price: 1.92,
+  },
+];
+
+const DetailOldCommand = () => {
+  const { id } = useParams();
+
+  const [listOfProducts, setListOfProducts] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    setListOfProducts(products);
+  }, []);
+
+  useEffect(() => {
+    let price = 0;
+    listOfProducts.map((item) => (price = price + item.price));
+    setTotalPrice(price);
+  }, [listOfProducts]);
+
+  return (
+    <>
+      <div className={styles.container}>
+        <h1>
+          Commande n°{id}, fait le -- --{/*TODO date de la commande*/}
+        </h1>
+        <div className={styles.top}>
+          <div className={styles.list_container}>
+            {listOfProducts.map((item, index) => (
+              <div key={index}>
+                <div className={styles.left}>{item.name}</div>
+                <div className={styles.right}>{item.price}€</div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={styles.bottom}>
+          <h3 className={styles.title}>Résumé de la commande</h3>
+          <div className={styles.content}>
+            <div className={styles.nbOfItem}>
+              Nombre d'article : {listOfProducts.length}
+            </div>
+            <div className={styles.totalPrice}>{totalPrice.toFixed(2)} €</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DetailOldCommand;
