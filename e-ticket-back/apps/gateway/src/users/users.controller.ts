e32ce1711aa0389d39c0d94720d8b174661d3186
matthieu <campagne.matthieu@gmail.com>
2023-07-04 15:31:01 +0200

    Add security Add protection on routes Fix docker compose

--- e-ticket-back/apps/gateway/src/users/users.controller.ts
+++ e-ticket-back/apps/gateway/src/users/users.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
-import { JwtAuthGuard } from '@app/common';
+import { JwtAuthGuard, Roles } from '@app/common';
+import { AccessControlInterceptor } from '@app/common/interceptors/acccess-control.interceptor';
 
+@UseInterceptors(AccessControlInterceptor)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -14,6 +24,7 @@ export class UsersController {
 
   @Post('company')
   @UseGuards(JwtAuthGuard)
+  @Roles('ADMIN')
   async createUserCompany(@Body() createUserDto: CreateUserDto) {
     return this.usersService.createUserCompany(createUserDto);
   }
@@ -26,6 +37,7 @@ export class UsersController {
 
   @Get()
   @UseGuards(JwtAuthGuard)
+  @Roles('ADMIN')
   async getAllUsers() {
     return this.usersService.getAllUsers();
   }
